1deb42703deaed9bbd71032de6ef046d0f80c00c
hdhminh <minhhuynhdoanhoang@gmail.com>
2025-03-27 11:52:04 +0700

    Update apply.php

--- apply.php
+++ apply.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    // If not login, redirect to login page
+    header("Location: login.php?error=please_login");
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +18,6 @@
 
 <body class="applypage">
     <?php 
-        session_start();
         require_once("menu.inc.php"); ?>
     </br></br></br>
     <h1>Job Application Form</h1>
@@ -28,85 +38,6 @@
                     <label for="Position">Job Reference Number</label>
                     <input type="text" id="Position" name="position" required="required" placeholder="e.g: GD123"
                         maxlength="5" size="10" pattern="[a-zA-Z0-9]+" />
-                </p>
-            </fieldset>
-        </div>
-        <div class="animatedborders" id="personal_info">
-            <fieldset>
-                <!--Personal Information name and DoB-->
-                <legend>Personal Information</legend>
-                <p>
-                    <label for="FirstName">First Name</label>
-                    <input type="text" id="FirstName" name="First_Name" maxlength="20" size="40" required="required"
-                        placeholder="First Name" pattern="[a-zA-Z ]+" />
-                </p>
-                <p>
-                    <label for="LastName">Last Name</label>
-                    <input type="text" id="LastName" name="Last_Name" maxlength="20" size="40" required="required"
-                        placeholder="Last Name" pattern="[a-zA-Z ]+" />
-                </p>
-                <p>
-                    <label for="DateOfBirth">Date Of Birth</label>
-                    <input type="text" id="DateOfBirth" name="DOB" required="required" placeholder="DD/MM/YYYY"
-                        pattern="\d{2}/\d{2}/\d{4}" />
-                </p>
-                <p>
-                    <label for="Email">Email</label>
-                    <input type="email" id="Email" name="Email" required="required" placeholder="name@example.com" />
-                </p>
-                <p>
-                    <label for="PhoneNumber">Phone Number</label>
-                    <input type="text" id="PhoneNumber" name="Phone" required="required" placeholder="37252525"
-                        pattern="[0-9]+">
-                </p>
-                <p id="genders">
-                    <label for="genders">Gender</label>
-                <div class="gender-options">
-                    <input type="radio" id="male" name="Gender" value="Male" checked="checked" required />
-                    <label for="male">Male</label>
-
-                    <input type="radio" id="female" name="Gender" value="Female" />
-                    <label for="female">Female</label>
-
-                    <input type="radio" id="others" name="Gender" value="Others" />
-                    <label for="others">Others</label>
-                </div>
-                </p>
-
-            </fieldset>
-        </div>
-        <div class="animatedborders">
-            <fieldset>
-                <!--Adress street,surburb/town text and drop down menu state, postcode text and allow 4 number-->
-                <legend>Address</legend>
-                <p>
-                    <label for="StreetAddress">Street Address</label>
-                    <input type="text" id="StreetAddress" name="Street_Address" maxlength="40" size="60"
-                        required="required" placeholder="Street Address" />
-                </p>
-                <p>
-                    <label for="SuburbTown">Suburb/Town</label>
-                    <input type="text" id="SuburbTown" name="Suburb/town" maxlength="40" size="60" required="required"
-                        placeholder="Suburb/Town" />
-                </p>
-                <p>
-                    <label for="State">State</label>
-                    <select name="state" id="State" required="required">
-                        <option value="" disabled selected>Please Select Your State</option>
-                        <option value="VIC">VIC</option>
-                        <option value="NSW">NSW</option>
-                        <option value="QLD">QLD</option>
-                        <option value="NT">NT</option>
-                        <option value="WA">WA</option>
-                        <option value="SA">SA</option>
-                        <option value="TAS">TAS</option>
-                        <option value="ACT">ACT</option>
-                    </select>
-                </p>
-                <p>
-                    <label for="Postcode">Post Code</label>
-                    <input type="text" id="Postcode" name="Postcode" maxlength="4" size="8" required="required"
-                        placeholder="Post Code" pattern="[0-9]+" />
                 </p>
             </fieldset>
         </div>
